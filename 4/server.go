dddc9fdb6fd34220648fc90afc6ef5aea9588b38
George Burgess IV <georgegburgess@gmail.com>
2013-10-12 04:58:51 -0400

    Finished website 4, i.e. 3 with HTTPS.

---
+++ 4/server.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"bytes"
+	"crypto/tls"
+	"errors"
+	"github.com/hoisie/web"
+	"html/template"
+	"io/ioutil"
+	"log"
+)
+
+var pages *template.Template
+
+var users = map[string]string{
+	"root":   "toor",
+	"george": "yolo",
+}
+
+func tryLogin(ctx *web.Context) (username string, err error) {
+	username, ok := ctx.Params["user"]
+	if !ok {
+		return "", errors.New("Need to give a username")
+	}
+
+	password, ok := ctx.Params["pass"]
+	if !ok {
+		return "", errors.New("Need to give a password")
+	}
+
+	userPassword, ok := users[username]
+	if !ok || userPassword != password {
+		return "", errors.New("Incorrect password.")
+	}
+	return username, nil
+}
+
+func loginPage(ctx *web.Context) string {
+	showLoginError := false
+	if ctx.Request.Method == "POST" {
+		name, err := tryLogin(ctx)
+		if err != nil {
+			showLoginError = true
+		} else {
+			// So secure, I know. Look into SetSecureCookie if you want
+			// logins that actually work.
+			ctx.SetCookie(web.NewCookie("user", name, 2400))
+			ctx.Redirect(302, "/home")
+			return "Redirecting you to /home..."
+		}
+	}
+
+	result := bytes.NewBuffer(nil)
+	pages.ExecuteTemplate(result, "login.html", showLoginError)
+	return result.String()
+}
+
+func printPage(ctx *web.Context) string {
+	username, err := ctx.Request.Cookie("user")
+
+	// No username cookie? Cool. Redirect to login.
+	if err != nil || username.Value == "" {
+		ctx.SetCookie(web.NewCookie("user", "", -1))
+		ctx.Redirect(302, "/login")
+		return "Redirecting to /login..."
+	}
+
+	result := bytes.NewBuffer(nil)
+	pages.ExecuteTemplate(result, "home.html", username.Value)
+	return result.String()
+}
+
+func main() {
+	var err error
+	pages, err = template.ParseFiles("login.html", "home.html")
+	if err != nil {
+		log.Fatal("Could not parse template files properly:", err)
+	}
+	web.Get("/favicon.ico", func() string { return "" })
+	web.Get("/login", loginPage)
+	web.Post("/login", loginPage)
+	web.Get("/home", printPage)
+
+	// HTTPS
+	pkey, err := ioutil.ReadFile("privkey.key")
+	if err != nil {
+		log.Fatal("Could not read privkey", err)
+	}
+
+	cert, err := ioutil.ReadFile("cert.cert")
+	if err != nil {
+		log.Fatal("Could not read cert", err)
+	}
+
+	keypair, err := tls.X509KeyPair(cert, pkey)
+	if err != nil {
+		log.Fatal("Unable to create key pair", err)
+	}
+
+	cfg := tls.Config{
+		Time:         nil,
+		Certificates: []tls.Certificate{keypair},
+	}
+
+	web.RunTLS("0.0.0.0:8081", &cfg)
+}
